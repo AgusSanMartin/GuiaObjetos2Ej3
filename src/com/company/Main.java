33fd4dff43c731c2419081c42bb4ace99d377dd7
Agustin <agus.sanmartin95@gmail.com>
2020-04-08 02:38:54 -0300

    Thread Sleep Added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,8 +14,11 @@ public class Main {
                                                                                     cli.getNomYape(),
                                                                                     cli.getGenero());
 
+
+
         Cuenta c1 = new Cuenta(10000,cli);
         c1.depositar(2000);
+        Thread.sleep(1000);
         Thread.sleep(1000);
         c1.extraer(500);
         Thread.sleep(1000);
